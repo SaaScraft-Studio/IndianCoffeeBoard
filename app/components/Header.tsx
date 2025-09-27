b585c6c470948947f810ba9d1520817bdd3b2c74
msaquibDev <md.saquib@saascraft.studio>
2025-09-27 19:22:14 +0530

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="w-full shadow-sm ">
-      <div className="relative w-full h-auto">
+      <div className="relative w-full h-auto ">
         <Image
           src="/coffeeHeader.jpg"
           alt="India International Coffee Festival 2026 - Coffee Board"
